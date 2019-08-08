8f7a82d1850054234e206dbde81c6f340ca9c222
158Alfetta <32899555+158Alfetta@users.noreply.github.com>
2019-08-09 06:35:29 +0700

    Ultrasonic test

---
+++ sketch_02.ino
@@ -0,0 +1,31 @@
+#define TRIGGER_PIN  D5
+#define ECHO_PIN     D4
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(TRIGGER_PIN, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+  // ความเร็วเสียงในอากาศประมาณ 340 เมตร/วินาที หรือ 29 ไมโครวินาที/เซนติเมตร
+  // ระยะทางที่ส่งเสียงออกไปจนเสียงสะท้อนกลับมาสามารถใช้หาระยะทางของวัตถุได้
+  // เวลาที่ใช้คือ ระยะทางไปกลับ ดังนั้นระยะทางคือ ครึ่งหนึ่งของที่วัดได้
+  
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(1000);
+  
+
+
+  
+}
